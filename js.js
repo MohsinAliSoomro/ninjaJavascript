f10f6709fc82c2fe0ee8da6e16779990c5b58ae0
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-08-05 13:46:55 +0500

    try funtion return object values explicitly

--- js.js
+++ js.js
@@ -15,3 +15,31 @@ var ninja2 = new Ninja();
 console.log(console.assert(ninja1.stulk === ninja1,"True", "he first ninja is stulking"))
 
 console.log(console.assert(ninja2.stulk === ninja2,"True", "the socond ninja is stulking"))
+
+//initialize a function and return as primitive value 
+function Ninja() {
+    //constructor
+    this.stulk = function () {
+        return this
+    };
+    return 1;
+}
+console.log(Ninja())
+
+//return a function method stalk
+var ninja3 = new Ninja()
+console.log(ninja3)
+
+//constructor explicitly return object values
+
+//Objects
+var poppet={
+    rules:false
+};
+//function
+function Emperor(){
+    this.rules=true;
+    return poppet;
+}
+var emp=new Emperor()
+console.log(emp)
