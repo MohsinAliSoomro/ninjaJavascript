b976f1206ebace5e5a28d96630fec800a8090f9a
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-08-05 17:07:32 +0500

    binding a foreach funtion

--- js.js
+++ js.js
@@ -1,28 +1,16 @@
-//Secrets of Javascript ninja
-
-//invode as a function 
-// 4.5 
-
-//function without strict mode
-function ninja() {
-    return this;
+function ForEach(item, callback) {
+    for (let n = 0; n < item.length; n++) {
+        callback.call(item[n], n)
+    }
 }
-console.log(ninja())
+var weapons = [
+    { type: 'Mohsin' },
+    { type: 'Ali' },
+    { type: 'Soomro' },
+]
 
-
-//function with strict mode
-function sumurai() {
-    "use strict"
-    return this;
-}
-console.log(sumurai())
-
-
-
-//As expected, a nonstrict function has window as function context
-console.log(console.assert(ninja() === window,"the context is global object"))
-
-
-//the strict function on the other hand has undefined context
-console.log(console.assert(sumurai() === undefined,"the context is undefined"));
-
+ForEach(weapons, function (index) {
+    if (this === weapons[index]) {
+        console.log(weapons[index].type)
+    }
+})
